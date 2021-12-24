b19d5d09274a5501e15635891016f6e129b6f25b
Mari Rider <87155577+mari-rider@users.noreply.github.com>
2021-12-23 19:49:00 -0500

    PiggyBank.swift

---
+++ PiggyBank.swift
@@ -0,0 +1,9 @@
+var pesos: Double = 20.63
+var reais: Double = 5.68
+var soles: Double = 4.01
+
+var total: Double = 0
+
+total = 20.63 * pesos + 5.68 * reais + 4.01 * soles
+
+print("US Dollars = \(total)")
